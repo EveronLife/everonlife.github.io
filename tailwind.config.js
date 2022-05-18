858fadbf8248ed0ccfffaa74d76015fdee232a82
Ryan <ryantorzynski@gmail.com>
2022-05-18 20:50:25 +0200

    Website files

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        './**/*.{razor,html}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'brandgreen': '#A7F413'
+            },
+        }
+    },
+    //variants: {
+    //    extend: {
+    //        fontWeight: ['hover'],
+    //        backgroundColor: ['active']
+    //    },
+    //},
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ],
+}
